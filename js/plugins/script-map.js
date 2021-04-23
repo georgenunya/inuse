c776f5c0d87b5cec294d60cbd392c01c750b0a0d
georgenunya <george@nunyagroup.com>
2021-04-23 13:50:17 +0100

    new

--- js/plugins/script-map.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery("#map1566739327259493916").simplegmaps({
-        MapOptions: {
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoom: 14,
-            scrollwheel: false,
-        }
-    });
-});
