f1fde90d4af1013d1b98122518008d832f48b121
georgenunya <george@nunyagroup.com>
2021-04-27 16:50:34 +0100

    bew

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -54,12 +54,11 @@
             zoomControl: false,
             styles: styles
         };
-
         function initMap() {
 			"use strict";
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);
             var bounds = new google.maps.LatLngBounds();
-            var myIcon = new google.maps.MarkerImage("images/icons/location-pin-3.png", null, null, null, new google.maps.Size(48, 48));
+            var myIcon = new google.maps.MarkerImage("https://img.icons8.com/ios-filled/100/000000/map-pin.png", null, null, null, new google.maps.Size(48, 48));
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(51.64579065680081, 0.4501208586616973),
                 map: map,
@@ -67,7 +66,6 @@
                 icon: myIcon
             });
         }
-
         initMap();
         google.maps.event.addDomListener(window, 'load', initMap);
     }
